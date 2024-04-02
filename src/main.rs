6468055f6f8e715c18639bdb7d1e907a98ba89bd
jwt2706 <jwt2home@gmail.com>
2024-04-02 08:41:24 -0400

    ascii + handle args + options menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::env;
+use dialoguer::{Select, theme::ColorfulTheme};
+use crossterm::cursor::Show;
+use crossterm::ExecutableCommand;
+use std::io::stdout;
+
 fn main() {
-    println!(r#"
+    ascii();
+    println!("v{}", env!("CARGO_PKG_VERSION"));
+    let args: Vec<String> = env::args().collect();
+    let _ = stdout().execute(Show);
+
+    if args.len() > 1 {
+        handle_args(&args[1]);
+    } else {
+        display_menu();
+    }
+}
+
+fn ascii(){
+    print!(r#"
 +==========================================+
 |  _____                 ____ ____   ____  |
 | | ____|__ _ ___ _   _ / ___|  _ \ / ___| |
@@ -8,7 +27,40 @@ fn main() {
 | |_____\__,_|___/\__, |\____|_|    \____| |
 |                 |___/                    |
 +==========================================+
-v0.1.0
-    "#);
-    println!("Hello, world!");
+"#);
+}
+
+
+fn handle_args(arg: &str) {
+    match arg {
+        "-e" => encrypt(),
+        "-d" => decrypt(),
+        _ => println!("Invalid argument. Please use -e for encryption or -d for decryption."),
+    }
+}
+
+fn display_menu() {
+    let selections = &["Encrypt", "Decrypt"];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Please choose your operation. Select with space key and confirm with space.")
+        .default(0)
+        .items(selections)
+        .interact()
+        .unwrap();
+
+    match selection {
+        0 => encrypt(),
+        1 => decrypt(),
+        _ => unreachable!(),
+    }
+}
+
+fn encrypt() {
+    println!("Encryption mode selected");
+    // Add your encryption code here
+}
+
+fn decrypt() {
+    println!("Decryption mode selected");
+    // Add your decryption code here
 }
